8d1ebe8e4dfc16fa5c34ca544db8237879769f25
nonsaucee <nishatkhan20431@htomail.ca>
2023-08-13 14:03:35 -0400

    Added autoscroll to last page

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
-import React, { useState } from 'react';
+import React, { useState, useRef, useEffect } from 'react';
 import './App.css';
 import NotebookPage from './NotebookPage';
 
 function App() {
     const [pages, setPages] = useState([{}]);
+    const pagesContainerRef = useRef(null);
 
     const addPage = () => {
         setPages([...pages, {}]);
@@ -15,11 +16,18 @@ function App() {
         setPages(newPages);
     }
 
+    // Scroll to the latest page when a new one is added
+    useEffect(() => {
+      if (pagesContainerRef.current) {
+          pagesContainerRef.current.scrollLeft = pagesContainerRef.current.scrollWidth;
+      }
+    }, [pages]); // This effect runs whenever the 'pages' state changes
+
     return (
         <div className="app-container">
             <h2 className="title">♡nishat's journal♡</h2>
             <div className="notebook">
-                <div className="pages-container">
+                <div className="pages-container" ref={pagesContainerRef}>
                     {pages.map((_, index) => (
                         <NotebookPage key={index} onDelete={() => deletePage(index)} />
                     ))}
