a95807c4c17907f11c7011366bbac1f38679484b
nonsaucee <nishatkhan20431@htomail.ca>
2023-08-13 20:49:08 -0400

    Added type onto new page automatically - needs fixing

--- src/App.js
+++ src/App.js
@@ -10,12 +10,6 @@ function App() {
         setPages([...pages, {}]);
     }
 
-    // const deletePage = (index) => {
-    //     const newPages = [...pages];
-    //     newPages.splice(index, 1);
-    //     setPages(newPages);
-    // }
-
     const deletePage = (index) => {
       const isLastPage = index === pages.length - 1;
       const newPages = [...pages];
@@ -23,10 +17,9 @@ function App() {
       
       setPages(newPages);
   
-      // Introduce a slight delay to ensure React has completed its rendering
       setTimeout(() => {
           if (isLastPage && pagesContainerRef.current) {
-              const pageWidth = 550; // Adjust this to the actual width of your NotebookPage component
+              const pageWidth = 612; // Adjust this to the actual width of your NotebookPage component
               const newScrollPosition = pagesContainerRef.current.scrollLeft - pageWidth;
   
               pagesContainerRef.current.scrollTo({
@@ -36,8 +29,6 @@ function App() {
           }
       }, 0);
     }
-  
-  
 
     // Scroll to the latest page when a new one is added
     useEffect(() => {
@@ -49,13 +40,46 @@ function App() {
       }
     }, [pages]);
 
+    const textareaRefs = useRef([]);
+
+    // Ensure the length of textareaRefs matches the pages
+    if (textareaRefs.current.length !== pages.length) {
+        textareaRefs.current = [...new Array(pages.length)].map(
+            (_, i) => textareaRefs.current[i] || React.createRef()
+        );
+    }
+
+    useEffect(() => {
+      textareaRefs.current.forEach((ref, index) => {
+          const handleInput = () => {
+              if (ref.current.scrollHeight > ref.current.clientHeight) {
+                  if (index === pages.length - 1) {  // If it's the last page
+                      addPage();
+                  } else {
+                      textareaRefs.current[index + 1].current.focus();
+                  }
+              }
+          };
+
+          if (ref.current) {
+              ref.current.addEventListener('input', handleInput);
+          }
+          
+          return () => {
+              if (ref.current) {
+                  ref.current.removeEventListener('input', handleInput);
+              }
+          };
+      });
+  }, [pages]);
+
     return (
         <div className="app-container">
             <h2 className="title">♡nishat's journal♡</h2>
             <div className="notebook">
                 <div className="pages-container" ref={pagesContainerRef}>
                     {pages.map((_, index) => (
-                        <NotebookPage key={index} onDelete={() => deletePage(index)} />
+                        <NotebookPage key={index} onDelete={() => deletePage(index)} textareaRef={textareaRefs.current[index]}/>
                     ))}
                 </div>
                 <button onClick={addPage} className="add-page-btn">→</button>
