257a8c6f26a759926909aaa179a3d88829b79753
nonsaucee <nishatkhan20431@htomail.ca>
2023-09-19 15:46:43 -0400

    Added auto-correct for spelling and grammar, ability to tab, working on ability to add bullet points

--- src/NotebookPage.js
+++ src/NotebookPage.js
@@ -1,21 +1,45 @@
 import React, { useEffect } from 'react';
 import './NotebookPage.css';
 import grammarCheck from './grammarCheck';
-// import autoCorrect from './autoCorrect';
 
-const NotebookPage = ({ onDelete, textareaRef }) => {
+const NotebookPage = ({ onDelete, textareaRef, updatePage }) => {
 
     useEffect(() => {
         if (textareaRef.current) {
           grammarCheck(textareaRef.current);
-        //   autoCorrect(textareaRef.current);
         }
       }, [textareaRef]);
 
+    const handleContextMenu = (e) => {
+      e.preventDefault();
+      const menu = document.getElementById("bullet-menu");
+      menu.style.left = `${e.clientX}px`;
+      menu.style.top = `${e.clientY}px`;
+      menu.style.display = "block";
+    };
+  
+    const handleMenuClick = () => {
+      // Logic for bullet point addition goes here later
+      const menu = document.getElementById("bullet-menu");
+      menu.style.display = "none";
+    };
+
     return (
         <div className="page-container">
-            <button className="delete-btn" onClick={onDelete}>✖</button>
-            <textarea className="lined-paper" ref={textareaRef}></textarea>
+          <div 
+            id="bullet-menu" 
+            className="bullet-menu" 
+            onClick={handleMenuClick}
+          >
+            <div className="bullet-menu-item">Bullet</div>
+          </div>
+          <button className="delete-btn" onClick={onDelete}>✖</button>
+          <textarea 
+            className="lined-paper" 
+            ref={textareaRef}
+            onChange={e => updatePage(e.target.value)} 
+            onContextMenu={handleContextMenu} 
+          />
         </div>
     );
 }
--- src/grammarCheck.js
+++ src/grammarCheck.js
@@ -1,11 +1,41 @@
 function grammarCheck(textarea) {
     textarea.addEventListener('input', function () {
+
       let text = this.value;
+
+      // Capitalize the first letter of a sentence
       text = text.replace(/([.!?]\s+)([a-z])/g, function (match, p1, p2) {
         return p1 + p2.toUpperCase();
       });
+
+      // Capitalize the first letter of the entire text
       text = text.charAt(0).toUpperCase() + text.slice(1);
+
+      // Auto-correct 
+      text = text.replace(/\bi(?=\s)/g, 'I');
+      text = text.replace(/\bwont\b/gi, "won't");
+      text = text.replace(/\bdont\b/gi, "don't");
+      text = text.replace(/\bthats\b/gi, "that's");
+      text = text.replace(/\bwhats\b/gi, "what's");
+      text = text.replace(/\bwiht\b/gi, "with");
+      text = text.replace(/\bte\b/gi, "the");
+
       this.value = text;
+    });
+
+    textarea.addEventListener('keydown', function (e) {
+      if (e.key === 'Tab') {
+        e.preventDefault();  // prevent moving focus to next element
+  
+        const start = this.selectionStart;
+        const end = this.selectionEnd;
+  
+        // Insert the tab character at the current cursor position
+        this.value = this.value.substring(0, start) + "\t" + this.value.substring(end);
+  
+        // Move the cursor to the position right after the tab character
+        this.selectionStart = this.selectionEnd = start + 1;
+      }
     });
   }
   
